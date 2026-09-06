5c70029e578084430c4e8fad874b484f5a30db2b
Candyfair <candice.bastien@gmail.com>
2026-09-06 21:16:10 +0200

    Updated keywords for task routage + README

--- run-digest.sh
+++ run-digest.sh
@@ -2,7 +2,7 @@
 osascript -e '
 tell application "Hyper" to activate
 tell application "System Events"
-  keystroke "cd /Users/candice/DEV/obsidian-scripts && /Users/candice/.nvm/versions/node/v24.11.1/bin/node index.js"
+  keystroke "cd /Users/candice/DEV/obsidian-scripts && /Users/candice/.nvm/versions/node/v24.11.1/bin/node index.js > /Users/candice/DEV/obsidian-scripts/logs/dock-debug.log 2>&1"
   key code 36
 end tell
 '
